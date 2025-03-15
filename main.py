d09dcd7144f687c39768140559e92f87ad49fc87
Albert França Josuá Costa <122242494+albertfrancajosuacosta@users.noreply.github.com>
2025-03-14 21:14:14 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import engine, Base
-import routes
-
-# Criar tabelas no banco
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-app.include_router(routes.router)
