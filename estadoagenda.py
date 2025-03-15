7103517f8fa585300240f5ff2bac6fcecc4218a2
Albert França Josuá Costa <122242494+albertfrancajosuacosta@users.noreply.github.com>
2025-03-14 21:13:54 -0300

    Delete estadoagenda.py

--- estadoagenda.py
+++
@@ -1,7 +0,0 @@
-import enum
-#Definição dos estados da agenda
-class EstadoAgenda(str, enum.Enum):
-    RECEBIDO = "RECEBIDO"
-    CONFIRMADO = "CONFIRMADO"
-    ATENDIDO = "ATENDIDO"
-    CANCELADO = "CANCELADO"
